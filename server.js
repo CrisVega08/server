69bcbc8c33ae2b3fdb3e1cd8d26184a67db20c03
Cristian <cristian>
2018-05-05 15:34:41 -0500

    images

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json())
 // configurar headers
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', 'X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization')
+    res.header('Access-Control-Allow-Headers', 'X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization, Referrer')
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
     res.header('Allow', 'GET, POST, OPTIONS, POST, DELETE')
 
